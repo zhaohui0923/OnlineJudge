4338a00245c1a422efe4e384e7af3082f72aa579
zhaohui_win10 <295493195@qq.com>
2020-01-10 10:48:20 +0800

    LeetCode121BestTimeToBuyAndSellStock

---
+++ LeetCode/121.BestTimeToBuyAndSellStock/121.BestTimeToBuyAndSellStock.cpp
@@ -0,0 +1,52 @@
+﻿// 121.BestTimeToBuyAndSellStock.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        // maxPrice[i]: maxPrice of range [i + 1, prices.size())
+        // i: [0: prices.size())
+        std::vector<int> maxPrice(prices.size(), 0);
+        // (maxPrice[i], maxPrice[prices.size() - 1] done
+        for (ptrdiff_t i = prices.size() - 1; i >= 0; --i) {
+            if (i == prices.size() - 1) {
+                maxPrice[i] = 0;
+            }
+            else {
+                maxPrice[i] = std::max(maxPrice[i + 1], prices[i + 1]);
+            }
+        }
+        // [maxPrice[0], maxPrice[prices.size() - 1] done
+
+        int maxProfit = 0;
+        // buyDay: [0, buyDay) tried 
+        for (ptrdiff_t buyDay = 0; buyDay < prices.size(); ++buyDay) {
+            // find maxPrice in [buyDay + 1, prices.size())
+            int tmpMaxPrice = maxPrice[buyDay];
+            maxProfit = std::max(maxProfit, tmpMaxPrice - prices[buyDay]);
+        }
+        // buyDay: [0, prices.size()) tried
+        return maxProfit;
+    }
+};
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门提示: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
